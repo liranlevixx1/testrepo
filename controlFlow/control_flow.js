6832663f2b6f4064fa94d9aa4129e49545ca9d61
Liran <liranlevixx1@gmail.com>
2024-12-04 03:09:46 -0500

    message

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,48 @@
+let userRole = "admin";
+let accessLevel;
+
+//verify the access level of that user
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+
+console.log("Access Level:", accessLevel);
+
+let isLoggedIn = true;
+let userMessage;
+//validates the login state
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
+//prints the message in the console
+console.log("User Message:", userMessage);
+
+
+let userType = "subscriber";
+let userCategory;
+//looking for the string to match then excecutes
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+console.log("User Category:", userCategory);
