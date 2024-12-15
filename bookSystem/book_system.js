106e13353390cf8d28b14f90fda80fdf788a01c0
Liran <liranlevixx1@gmail.com>
2024-12-15 14:25:33 -0500

    message

---
+++ bookSystem/book_system.js
@@ -0,0 +1,49 @@
+  //declaring an emoty array for books
+  let books = [];
+  
+  //adds books from the user's inputs
+  function addBook() {
+            const bookName = document.getElementById('bookName').value;
+            const authorName = document.getElementById('authorName').value;
+            const bookDescription = document.getElementById('bookDescription').value;
+            const pagesNumber = parseInt(document.getElementById('pagesNumber').value);
+            if (bookName && authorName && bookDescription && !isNaN(pagesNumber)) {
+                const book = {
+                    name: bookName,
+                    authorName: authorName,
+                    bookDescription: bookDescription,
+                    pagesNumber: pagesNumber
+                };
+                books.push(book);
+                showbooks();
+                clearInputs();
+            } else {
+                alert('Please fill in all fields correctly.');
+            }
+        }
+    // show book details in the management system
+    function showbooks() {
+    const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
+        <p><strong>Book Name: </strong>${book.name}</p>
+        <p><strong>Author Name:</strong> ${book.authorName}</p>
+        <p><strong>Book Description:</strong> ${book.bookDescription}</p>
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <button onclick="deleteBook(${index})">Delete</button>`
+    );
+    document.getElementById('books').innerHTML = booksDiv.join('');
+}
+
+// clears the user's inputs when called
+ function clearInputs() {
+            document.getElementById('bookName').value = '';
+            document.getElementById('authorName').value = '';
+            document.getElementById('bookDescription').value = '';
+            document.getElementById('pagesNumber').value = '';
+ }
+
+ //removing the book
+ function deleteBook (index){
+    books.splice(index, 1);
+    //rerendering the books view
+    showbooks(); 
+ }
